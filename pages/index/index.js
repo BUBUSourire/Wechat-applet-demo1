d551bf8d545f7b6c3fc65f66d9dde4b9829271a7
Sourire <953810390@qq.com>
2019-10-31 22:35:59 +0800

    接口引入

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,5 +3,29 @@
 const app = getApp()
 
 Page({
-  
+    data: {
+        num: 0,
+        items: []
+    },
+    onLoad() {
+        wx.request({
+            url: 'https://locally.uieee.com/slides',
+            header: {
+                'content-type': 'application/json'
+            },
+            method: "GET",
+            success: (res) => {
+                console.log(res)
+                // this.data.items = res.data
+                /*
+                this.setData两个功能：
+                1.更新数据
+                2.更新视图
+                */
+                this.setData({
+                    items: res.data
+                })
+            }
+        })
+    }
 })
